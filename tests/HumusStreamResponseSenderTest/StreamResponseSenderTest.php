5302deb8b0a827edf1a964c26bcb8d59eebfaf39
prolic <saschaprolic@googlemail.com>
2013-06-29 22:15:09 +0200

    add tests for default streaming mode

--- tests/HumusStreamResponseSenderTest/StreamResponseSenderTest.php
+++ tests/HumusStreamResponseSenderTest/StreamResponseSenderTest.php
@@ -18,12 +18,72 @@
 
 namespace HumusStreamResponseSenderTest;
 
+use HumusStreamResponseSender\StreamResponseSender;
 use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Http\Headers;
+use Zend\Http\Response\Stream;
 
 class StreamResponseSenderTest extends TestCase
 {
-    public function testTravis()
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendHeadersInDefaultMode()
     {
-        $this->assertTrue(true);
+        if (!function_exists('xdebug_get_headers')) {
+            $this->markTestSkipped('Xdebug extension needed, skipped test');
+        }
+
+        $testFile = __DIR__ . '/TestAsset/sample-stream-file.txt';
+        $fileSize = filesize($testFile);
+        $basename = basename($testFile);
+        $stream = fopen($testFile, 'rb');
+
+        $headers = array(
+            'Content-Disposition: attachment; filename="' . $basename . '"',
+            'Content-Type: application/octet-stream',
+        );
+
+        $response = new Stream();
+        $response->setStream($stream);
+        $response->setContentLength($fileSize);
+        $response->setStreamName($basename);
+        $response->getHeaders()->addHeaders($headers);
+
+
+        $mockSendResponseEvent = $this->getMock(
+            'Zend\Mvc\ResponseSender\SendResponseEvent',
+            array('getResponse')
+        );
+        $mockSendResponseEvent->expects(
+            $this->any())
+            ->method('getResponse')
+            ->will($this->returnValue($response)
+            );
+
+        $requestMock = $this->getMockForAbstractClass('Zend\Http\Request');
+
+        $responseSender = new StreamResponseSender();
+        $responseSender->setRequest($requestMock);
+
+        $responseSender->sendHeaders($mockSendResponseEvent);
+
+        $expectedHeaders = array_merge($headers, array(
+            'Content-Transfer-Encoding: binary',
+            'Accept-Ranges: bytes',
+            'Content-Range: bytes 0-' . ($fileSize - 1) . '/' . $fileSize,
+            'Content-Length: ' . $fileSize
+
+        ));
+
+        $sentHeaders = xdebug_get_headers();
+
+        $diff = array_diff($sentHeaders, $expectedHeaders);
+
+        if (count($diff)) {
+            $header = array_shift($diff);
+            $this->assertContains('XDEBUG_SESSION', $header);
+            $this->assertEquals(0, count($diff));
+        }
     }
 }
